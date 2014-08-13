f8ef28fd04cc504b7dba4517e834c2b8847b8bdd
aharon hacmon <aharon.topq@gmail.com>
2014-08-13 14:35:34 +0300

    implementation draft #1

--- difido-testng/src/main/java/il/co/topq/integframework/reporting/TestNGReporter.java
+++ difido-testng/src/main/java/il/co/topq/integframework/reporting/TestNGReporter.java
@@ -1,27 +1,112 @@
 package il.co.topq.integframework.reporting;
 
+import static il.co.topq.difido.model.Enums.Status.*;
 import il.co.topq.difido.client.DifidoClient;
+import il.co.topq.difido.model.execution.MachineNode;
+import il.co.topq.difido.model.execution.ScenarioNode;
+import il.co.topq.difido.model.execution.TestNode;
+import il.co.topq.difido.model.test.TestDetails;
 import il.co.topq.report.Configuration;
 import il.co.topq.report.Configuration.ConfigProps;
 
-import org.testng.ITestContext;
-import org.testng.TestListenerAdapter;
+import java.util.List;
 
-public class TestNGReporter extends TestListenerAdapter {
+import org.testng.IReporter;
+import org.testng.ISuite;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.internal.IResultListener2;
+import org.testng.xml.XmlSuite;
+
+public class TestNGReporter extends org.testng.Reporter implements IReporter, IResultListener2 {
 
 	protected final DifidoClient client = DifidoClient.build(Configuration.INSTANCE.read(ConfigProps.BASE_URI));
-	int execution;
+	protected int executionId;
+	protected int machineId;
+	protected int scenarioId = -1;
+	protected int testId;
+
+	public TestNGReporter() {
+		executionId = client.addExecution();
+		machineId = client.addMachine(executionId, new MachineNode(System.getProperty("user.name")));
+
+	}
 
 	@Override
 	public void onStart(ITestContext testContext) {
-		super.onStart(testContext);
-		execution = client.addExecution();
+		if (scenarioId == -1) {
+			scenarioId = client.addRootScenario(executionId, machineId, new ScenarioNode(testContext.getSuite().getName()));
+		} else {
+			client.addSubScenario(executionId, machineId, scenarioId, new ScenarioNode(testContext.getSuite().getName()));
+		}
+
 	}
 
 	@Override
 	public void onFinish(ITestContext testContext) {
-		client.endExecution(execution);
-		super.onFinish(testContext);
+		client.endExecution(executionId);
+	}
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		TestNode node = new TestNode();
+		node.setStatus(in_progress);
+		testId = client.addTest(executionId, machineId, scenarioId, node);
+		TestDetails details = new TestDetails(result.getName());
+		details.setDescription(result.getTestName());
+		client.addTestDetails(executionId, machineId, scenarioId, testId, details);
+	}
+
+	@Override
+	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		client.getTest(executionId, machineId, scenarioId, testId).setStatus(success);
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		client.getTest(executionId, machineId, scenarioId, testId).setStatus(failure);
+
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		client.getTest(executionId, machineId, scenarioId, testId).setStatus(skipped);
+
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		client.getTest(executionId, machineId, scenarioId, testId).setStatus(warning);
+	}
+
+	@Override
+	public void onConfigurationSuccess(ITestResult itr) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onConfigurationFailure(ITestResult itr) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onConfigurationSkip(ITestResult itr) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void beforeConfiguration(ITestResult tr) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
